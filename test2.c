1a6cc4273d75a6d3501b195160ab0066837e77e1
EatByte <tusharwabale630@hotmail.com>
2019-08-18 09:51:48 +0530

    pr 7 alternate aproach

---
+++ test2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(void)
+{
+	char *s = "thar";
+	char *t = "tushar";
+	
+	if (s == t)
+	{
+		printf("same \n");
+	}
+	else
+	{
+		printf("differetn \n");
+	}
+}
