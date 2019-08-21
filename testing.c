3e5a52727c6c44d70894a129baf89fc6939de397
EatByte <tusharwabale630@hotmail.com>
2019-08-21 08:53:38 +0530

    Problem 11 formulated input

--- testing.c
+++ testing.c
@@ -4,24 +4,7 @@
 
 int main(void)
 {
-        char str[]="Deleting Feelings...";
-        int len=strlen(str);
-        for(int i=0;i<len;i++)
-        {
-                printf("%c",str[i]);
-                sleep(1);
-        }
-        printf("\n");
-        for(int i=1;i<=7;i++)
-        {
-                printf("[");
-                for(int k=1;k<=i;k++)
-                {
-                        printf("+");
-                        sleep(0.5);
-                }
-                printf("]%10d%\n",i*10);
-        }
-        printf("Failed!!\nError File too large!!\n");
-        return 0;
+	char *s = "12345";
+	int a = ((int)s[4] - 48) * 10;
+	printf("%i ", a);
 }
